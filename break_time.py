483a79ebf085f85c419b2d69b6d305f99dd379fb
teichopsia <anthomelanous@hotmail.com>
2015-08-03 19:47:12 -0500

    minor changes

--- break_time.py
+++ break_time.py
@@ -9,15 +9,18 @@
 
 # [1] Check literature for amount of breaks and time. more time worked,
 # longer breaks needed.
-import webbrowser, time
+import webbrowser
+import time
 
 total_breaks = 3
 break_count = 0
 
 url = 'https://www.youtube.com/watch?v=uQes2Bh9A3w'
 
+print("This program started on " + time.ctime())
+
 while break_count < total_breaks:
-    time.sleep(5) #in seconds
+    time.sleep(10)               #<- in seconds - 2 hours = time.sleep(2*60*60)
     webbrowser.open_new_tab(url)
     break_count += 1
     
